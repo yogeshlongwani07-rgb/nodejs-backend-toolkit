48ea3985dfe264effee5b115ccf4dca6a24cbb9b
yogeshlongwani07 <yogeshlongwani07@gmail.com>
2026-05-25 10:39:31 +0530

    update socket.io

--- Web-Socket/app.js
+++ Web-Socket/app.js
@@ -6,7 +6,14 @@ const path = require("path");
 const { Server } = require("socket.io");
 
 app.use(express.static(path.resolve("./public")));
-const io = new Server()
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+    socket.emit("message", msg);
+  });
+});
 
 app.get("/", (req, res) => {
   return res.sendFile("/public/index.html");
