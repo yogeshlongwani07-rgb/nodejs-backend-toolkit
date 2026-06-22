465cfac1ac5f1da164175e555aa283f70b690ccc
yogeshlongwani07 <yogeshlongwani07@gmail.com>
2026-06-22 19:14:07 +0530

    add unauthorized user check in admin and user authentication middleware

--- Machine-Coding-Task/Movie-Reservation-System/src/middleware/user-auth.js
+++ Machine-Coding-Task/Movie-Reservation-System/src/middleware/user-auth.js
@@ -1,4 +1,7 @@
 function isUser(req, res, next) {
+  if (!req.user) {
+    return res.status(401).json({ message: "Unauthorized", success: false });
+  }
   if (req.user.role !== "user")
     return res.status(403).json({
       message: "Only user can book Movies",
