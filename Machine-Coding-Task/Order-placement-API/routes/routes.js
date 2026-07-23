ffd9b042e0f54a6d848818796995a0c5224940ca
yogeshlongwani07 <yogeshlongwani07@gmail.com>
2026-07-24 02:25:56 +0530

    Remove package.json and URL routing logic from the URL Shortener project

--- Machine-Coding-Task/Order-placement-API/routes/routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const createProduct = require("../controllers/createproduct");
-
-router.post("/", createProduct);
-
-module.exports = router;
--- Machine-Coding-Task/Order-placement-API/routes/routes.order.js
+++
@@ -1,7 +0,0 @@
-const placeOrder = require("../controllers/createOrder");
-const express = require("express");
-const router = express.Router();
-
-router.post("/", placeOrder);
-
-module.exports = router;
